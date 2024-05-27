08ee5565b61424b43b9e9acd492b5db9e52ca32f
Francisco José Coto Pacheco <fcoto@uoc.edu>
2024-05-28 00:35:50 +0200

    P3 modified

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,3 +14,48 @@
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Index selectors
+  const overlay = document.querySelector('.overlay');
+
+  // Header selectors
+  const menuButton = document.querySelector('.header__menu-button');
+  const navContainer = document.querySelector('.header__nav');
+  const iconBar = document.querySelector('.fa-bars');
+  const iconTimes = document.querySelector('.fa-times');
+
+  function toggleMenu() {
+    if (navContainer.style.transform === 'translateX(-100%)' || !navContainer.style.transform) {
+      navContainer.style.transform = 'translateX(0%)';
+      iconBar.style.display = 'none';
+      iconTimes.style.display = 'block';
+      overlay.style.display = 'block';
+    } else {
+      navContainer.style.transform = 'translateX(-100%)';
+      iconBar.style.display = 'block';
+      iconTimes.style.display = 'none';
+      overlay.style.display = 'none';
+    }
+
+  }
+
+  menuButton.addEventListener('click', toggleMenu);
+
+  // Active menu option
+  const path = window.location.pathname;
+  const options = document.querySelectorAll('.header__nav-item > a');
+
+  options.forEach(function (option) {
+    if (option.getAttribute('href') === path) {
+      option.classList.add('active');
+    }
+
+    if (path === '/detail.html') {
+      options[1].classList.add('active');
+    }
+
+  });
+
+});

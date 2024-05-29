e83f86d96f98874d5ff41b5db452345247cd60a4
Francisco José Coto Pacheco <fcoto@uoc.edu>
2024-05-30 01:44:52 +0200

    P3 modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,26 @@ module.exports = {
       'blue': '#3a63df',
       'gray': '#f8f9fa',
       'gray-light': '#d3dce6',
+      'gray-dark':'#6c757d',
       'pink': '#fabada',
       'green': '',
     },
     extend: {
+      fontSize: {
+        '2.5xl': '1.75rem',
+      },
       boxShadow: {
         'pink': '0 0 0.5rem 0.5rem pink',
       },
+      width: {
+        '4/5': '80%',
+      },
+      maxWidth: {
+        '7.5xl': '85rem',
+        '8xl': '90rem',
+        '9xl': '100rem',
+        '10xl': '110rem',
+      }
     }
   },
   variants: {
